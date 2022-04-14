4feb0bac5b3b2733411f1e39f92028e6faacc3ec
long1712578 <pdlong578@gmail.com>
2022-04-15 01:46:50 +0700

    commit 2

---
+++ TMTDemoMongDb/Controllers/DepartmentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using TMTDemoMongDb.Models;
+
+namespace TMTDemoMongDb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("TdeskAppConn"));
+            var listDepartment = dbClient.GetDatabase("DemoTdeskDb").GetCollection<Department>("Department").AsQueryable();
+            return new JsonResult(listDepartment);
+        }
+    }
+}
